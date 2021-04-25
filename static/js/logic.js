c04c3a433c2d1c7a73e43e500f9e87c2e0a6d3b5
Tianchi Zhang <tianchi.zhang01@gmail.com>
2021-04-25 19:24:27 -0400

    Base and circle layers. Circle layers sizes vary. Color still needs to change based on earthquake depth.

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,171 +1,95 @@
-// Template code...
+// Function to determine circle radius based on earthquake magnitude
+function circleSize(magnitude) {
+    return magnitude * 20000;
+}
 
-// // Create the tile layer that will be the background of our map
-// var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-//   attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-//   maxZoom: 18,
-//   id: "light-v10",
-//   accessToken: API_KEY
-// });
+urlEarthquakes = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 
-// // Initialize all of the LayerGroups we'll be using
-// var layers = {
-//   COMING_SOON: new L.LayerGroup(),
-//   EMPTY: new L.LayerGroup(),
-//   LOW: new L.LayerGroup(),
-//   NORMAL: new L.LayerGroup(),
-//   OUT_OF_ORDER: new L.LayerGroup()
-// };
+d3.json(urlEarthquakes).then(function(dataEq) {
+    // Once we get a response, send the data.features object to the createFeatures function
+    createCircles(dataEq.features);
+});
 
-// // Create the map with our layers
-// var map = L.map("map-id", {
-//   center: [40.73, -74.0059],
-//   zoom: 12,
-//   layers: [
-//     layers.COMING_SOON,
-//     layers.EMPTY,
-//     layers.LOW,
-//     layers.NORMAL,
-//     layers.OUT_OF_ORDER
-//   ]
-// });
+// ------------------------------------------------------------
+// ------------------------------------------------------------
 
-// // Add our 'lightmap' tile layer to the map
-// lightmap.addTo(map);
+function createCircles(featuresEq) {
 
-// // Create an overlays object to add to the layer control
-// var overlays = {
-//   "Coming Soon": layers.COMING_SOON,
-//   "Empty Stations": layers.EMPTY,
-//   "Low Stations": layers.LOW,
-//   "Healthy Stations": layers.NORMAL,
-//   "Out of Order": layers.OUT_OF_ORDER
-// };
 
-// // Create a control for our layers, add our overlay layers to it
-// L.control.layers(null, overlays).addTo(map);
+    // Define an array to hold earthquake coordinates
+    var earthquakeCircles = [];
+    
+    for (var i = 0; i < featuresEq.length; i++) {
+        
+        var coordinates = featuresEq[i].geometry.coordinates
 
-// // Create a legend to display information about our map
-// var info = L.control({
-//   position: "bottomright"
-// });
+        earthquakeCircles.push(
+            L.circle([coordinates[1], coordinates[0]], {
+                weight: 0.5,
+                opacity: 0.3,
+                fillOpacity: 0.7,
+                color: "black",
+                fillColor: "green",
+                radius: circleSize(featuresEq[i].properties.mag)
+            })
+        );
+    }
 
-// // When the layer control is added, insert a div with the class of "legend"
-// info.onAdd = function() {
-//   var div = L.DomUtil.create("div", "legend");
-//   return div;
-// };
-// // Add the info legend to the map
-// info.addTo(map);
+    var earthquakesLayer = L.layerGroup(earthquakeCircles);
 
-// // Initialize an object containing icons for each layer group
-// var icons = {
-//   COMING_SOON: L.ExtraMarkers.icon({
-//     icon: "ion-settings",
-//     iconColor: "white",
-//     markerColor: "yellow",
-//     shape: "star"
-//   }),
-//   EMPTY: L.ExtraMarkers.icon({
-//     icon: "ion-android-bicycle",
-//     iconColor: "white",
-//     markerColor: "red",
-//     shape: "circle"
-//   }),
-//   OUT_OF_ORDER: L.ExtraMarkers.icon({
-//     icon: "ion-minus-circled",
-//     iconColor: "white",
-//     markerColor: "blue-dark",
-//     shape: "penta"
-//   }),
-//   LOW: L.ExtraMarkers.icon({
-//     icon: "ion-android-bicycle",
-//     iconColor: "white",
-//     markerColor: "orange",
-//     shape: "circle"
-//   }),
-//   NORMAL: L.ExtraMarkers.icon({
-//     icon: "ion-android-bicycle",
-//     iconColor: "white",
-//     markerColor: "green",
-//     shape: "circle"
-//   })
-// };
+    createMap(earthquakesLayer);
 
-// // Perform an API call to the Citi Bike Station Information endpoint
-// d3.json("https://gbfs.citibikenyc.com/gbfs/en/station_information.json").then(function(infoRes) {
+}
 
-//   // When the first API call is complete, perform another call to the Citi Bike Station Status endpoint
-//   d3.json("https://gbfs.citibikenyc.com/gbfs/en/station_status.json").then(function(statusRes) {
-//     var updatedAt = infoRes.last_updated;
-//     var stationStatus = statusRes.data.stations;
-//     var stationInfo = infoRes.data.stations;
 
-//     // Create an object to keep of the number of markers in each layer
-//     var stationCount = {
-//       COMING_SOON: 0,
-//       EMPTY: 0,
-//       LOW: 0,
-//       NORMAL: 0,
-//       OUT_OF_ORDER: 0
-//     };
+function createMap(earthquakesLayer) {
 
-//     // Initialize a stationStatusCode, which will be used as a key to access the appropriate layers, icons, and station count for layer group
-//     var stationStatusCode;
+    // Base layers
+    var satelliteMap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+        attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+        maxZoom: 18,
+        id: "satellite-v9",
+        accessToken: API_KEY
+    });
 
-//     // Loop through the stations (they're the same size and have partially matching data)
-//     for (var i = 0; i < stationInfo.length; i++) {
+    var lightMap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+        attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+        maxZoom: 18,
+        id: "light-v10",
+        accessToken: API_KEY
+    });
 
-//       // Create a new station object with properties of both station objects
-//       var station = Object.assign({}, stationInfo[i], stationStatus[i]);
-//       // If a station is listed but not installed, it's coming soon
-//       if (!station.is_installed) {
-//         stationStatusCode = "COMING_SOON";
-//       }
-//       // If a station has no bikes available, it's empty
-//       else if (!station.num_bikes_available) {
-//         stationStatusCode = "EMPTY";
-//       }
-//       // If a station is installed but isn't renting, it's out of order
-//       else if (station.is_installed && !station.is_renting) {
-//         stationStatusCode = "OUT_OF_ORDER";
-//       }
-//       // If a station has less than 5 bikes, its status is low
-//       else if (station.num_bikes_available < 5) {
-//         stationStatusCode = "LOW";
-//       }
-//       // Otherwise the station is normal
-//       else {
-//         stationStatusCode = "NORMAL";
-//       }
+    var outdoorsMap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+        attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+        maxZoom: 18,
+        id: "outdoors-v11",
+        accessToken: API_KEY
+    });
 
-//       // Update the station count
-//       stationCount[stationStatusCode]++;
-//       // Create a new marker with the appropriate icon and coordinates
-//       var newMarker = L.marker([station.lat, station.lon], {
-//         icon: icons[stationStatusCode]
-//       });
+    // Create a baseMaps object
+    var baseMaps = {
+        "Stellite Map": satelliteMap,
+        "Light Map": lightMap,
+        "Outdoors Map": outdoorsMap
+    };
 
-//       // Add the new marker to the appropriate layer
-//       newMarker.addTo(layers[stationStatusCode]);
+    // Create an overlay object
+    var overlayMaps = {
+    //   "State Population": states,
+    "Earthquakes": earthquakesLayer
+    };
 
-//       // Bind a popup to the marker that will  display on click. This will be rendered as HTML
-//       newMarker.bindPopup(station.name + "<br> Capacity: " + station.capacity + "<br>" + station.num_bikes_available + " Bikes Available");
-//     }
+    // Define a map object
+    var myMap = L.map("map", {
+        // center: [39.55, -105.78],
+        center: [47.61, -122.33],
+        zoom: 4,
+        layers: [satelliteMap, earthquakesLayer]
+    });
 
-//     // Call the updateLegend function, which will... update the legend!
-//     updateLegend(updatedAt, stationCount);
-//   });
-// });
-
-// // Update the legend's innerHTML with the last updated time and station count
-// function updateLegend(time, stationCount) {
-//   document.querySelector(".legend").innerHTML = [
-//     "<p>Updated: " + moment.unix(time).format("h:mm:ss A") + "</p>",
-//     "<p class='out-of-order'>Out of Order Stations: " + stationCount.OUT_OF_ORDER + "</p>",
-//     "<p class='coming-soon'>Stations Coming Soon: " + stationCount.COMING_SOON + "</p>",
-//     "<p class='empty'>Empty Stations: " + stationCount.EMPTY + "</p>",
-//     "<p class='low'>Low Stations: " + stationCount.LOW + "</p>",
-//     "<p class='healthy'>Healthy Stations: " + stationCount.NORMAL + "</p>"
-//   ].join("");
-// }
+    // Pass our map layers into our layer control
+    // Add the layer control to the map
+    L.control.layers(baseMaps, overlayMaps, {
+        collapsed: false
+    }).addTo(myMap);
+}
