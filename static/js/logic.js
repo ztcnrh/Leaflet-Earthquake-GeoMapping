c1a55a5e6e7a6da927f6894895551ced6838e755
Tianchi Zhang <tianchi.zhang01@gmail.com>
2021-04-25 20:52:43 -0400

    Add legend with colors

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,12 +1,23 @@
-// Function to determine circle radius based on earthquake magnitude
+// Function to scale circle radius to our desired circle size based on earthquake magnitude
 function circleSize(magnitude) {
     return magnitude * 20000;
 }
 
+// Function to assign a legend label color based on earthquake depths
+function getColor(d) {
+    return d > 90  ? '#ff320e' :
+           d > 70  ? '#fa6c1a' :
+           d > 50   ? '#fdab30' :
+           d > 30   ? '#f8d151' :
+           d > 10   ? '#e9ff22' :
+                      '#47ce25';
+}
+
+
 urlEarthquakes = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 
 d3.json(urlEarthquakes).then(function(dataEq) {
-    // Once we get a response, send the data.features object to the createFeatures function
+    // Once we get a response, send the features object to the createCircles() function
     createCircles(dataEq.features);
 });
 
@@ -14,7 +25,6 @@ d3.json(urlEarthquakes).then(function(dataEq) {
 // ------------------------------------------------------------
 
 function createCircles(featuresEq) {
-
 
     // Define an array to hold earthquake coordinates
     var earthquakeCircles = [];
@@ -32,19 +42,20 @@ function createCircles(featuresEq) {
                 fillColor: "green",
                 radius: circleSize(featuresEq[i].properties.mag)
             })
-        );
+        )
     }
 
+    // Group the circle objects array into a layer group
     var earthquakesLayer = L.layerGroup(earthquakeCircles);
 
+    // Pass the layer group to the createMap() function
     createMap(earthquakesLayer);
-
 }
 
 
 function createMap(earthquakesLayer) {
 
-    // Base layers
+    // Base layers to choose from
     var satelliteMap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
         attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
         maxZoom: 18,
@@ -73,7 +84,7 @@ function createMap(earthquakesLayer) {
         "Outdoors Map": outdoorsMap
     };
 
-    // Create an overlay object
+    // Create an overlayMaps object
     var overlayMaps = {
     //   "State Population": states,
     "Earthquakes": earthquakesLayer
@@ -82,14 +93,32 @@ function createMap(earthquakesLayer) {
     // Define a map object
     var myMap = L.map("map", {
         // center: [39.55, -105.78],
-        center: [47.61, -122.33],
+        center: [47.61, -122.33], //Seattle, WA geo coordinates
         zoom: 4,
         layers: [satelliteMap, earthquakesLayer]
     });
 
-    // Pass our map layers into our layer control
-    // Add the layer control to the map
+    // Add layer control to the map
     L.control.layers(baseMaps, overlayMaps, {
         collapsed: false
     }).addTo(myMap);
+
+    // Create a legend to display color categorizations of the earthquake depths
+    var legend = L.control({position: "bottomright"});
+
+    legend.onAdd = function (myMap) {
+        var div = L.DomUtil.create('div', 'info legend'),
+            depth = [-10, 10, 30, 50, 70, 90]
+    
+        // Loop through our density intervals (depth) and generate a label with a colored square for each interval
+        for (var i = 0; i < depth.length; i++) {
+            div.innerHTML +=
+                '<i style="background:' + getColor(depth[i] + 1) + '"></i> ' +
+                depth[i] + (depth[i + 1] ? '&ndash;' + depth[i + 1] + '<br>' : '+');
+        }
+    
+        return div;
+    }
+    
+    legend.addTo(myMap);
 }
